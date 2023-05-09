56574663a6b61b2ea38d23ba502b533ad5466a68
Anthony Bosek <anthony.bosek@gmail.com>
2023-05-09 13:15:03 -0700

    added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Awesome Algorithms
+
+////////////////////////////////////////////////////////////////
+
+# Development
+
+## Description
+
+This is a collection of algorithms that I have written in JavaScript. I have also included a test file for each algorithm.
+
+## Structure
+
+JavaScript syntax is used for all algorithms. Each algorithm is in its own file.
+
+### contributors
+
+- Anthony Bosek
+
+```
+
+```
--- edabitSolutions/objMod/app.js
+++ edabitSolutions/objMod/app.js
@@ -6,26 +6,48 @@
 //
 // ------------------------------------------------------------------------------------------------
 
-const objMod = (obj, action, prop, newValue) => {
-  if (action === "edit") {
-    if (obj.hasOwnProperty(prop)) {
-      obj[prop] = newValue;
-    } else {
-      return "Property not found";
-    }
-  } else if (action === "add") {
-    obj[prop] = newValue;
-  } else if (action === "delete") {
-    delete obj[prop];
-  }
-  return obj;
-}; //
+// const objMod = (obj, action, prop, newValue) => {
+//   if (action === "edit") {
+//     if (obj.hasOwnProperty(prop)) {
+//       obj[prop] = newValue;
+//     } else {
+//       return "Property not found";
+//     }
+//   } else if (action === "add") {
+//     obj[prop] = newValue;
+//   } else if (action === "delete") {
+//     delete obj[prop];
+//   }
+//   return obj;
+// }; //
 
-console.log(
-  objMod(
-    { name: "Mike", location: "Phoenix", job: "Plumber" },
-    edit,
-    "name",
-    "Anthony"
-  )
-);
+// console.log(
+//   objMod(
+//     { name: "Mike", location: "Phoenix", job: "Plumber" },
+//     edit,
+//     "name",
+//     "Anthony"
+//   )
+// );
+
+const stockOrder = () => {
+  const collection = [];
+
+  const add = (item) => {
+    collection.push(item);
+  };
+
+  const remove = (item) => {
+    collection = collection.filter((i) => i !== item);
+    // const index = collection.indexOf(item)
+    // if (index !== -1) {
+    //   collection.splice(index, 1)
+    // }
+  };
+
+  const list = () => {
+    return collection;
+  };
+
+  return { add, remove, list };
+};
